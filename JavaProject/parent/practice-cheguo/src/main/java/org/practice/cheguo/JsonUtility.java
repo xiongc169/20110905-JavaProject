81b2110d1153e47c90fdaf3acf4f6182a489a6ac
xiongchao <xiongchao@zafh.com.cn>
2018-03-13 20:27:59 +0800

    jsonToObject

--- JavaProject/parent/practice-cheguo/src/main/java/org/practice/cheguo/JsonUtility.java
+++ JavaProject/parent/practice-cheguo/src/main/java/org/practice/cheguo/JsonUtility.java
@@ -1,11 +1,25 @@
 package org.practice.cheguo;
 
+import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
+/**
+ * https://www.cnblogs.com/cdf-opensource-007/p/7106018.html
+ * 
+ * @author 20180112002
+ *
+ */
 public class JsonUtility {
 
 	public static void main(String[] args) {
 
+		// json();
+		jsonToMap();
+	}
+
+	public static void json() {
 		JSONObject isRegisteredObj = new JSONObject();
 		isRegisteredObj.put("registerMobile", "registerMobile11");
 		isRegisteredObj.put("reservedMobile", "reservedMobile22");
@@ -18,6 +32,31 @@ public class JsonUtility {
 		isRegisteredObj.put("reqSn", "reqSn99");
 		String jsonStr = isRegisteredObj.toJSONString();
 		System.out.println(jsonStr);
+	}
+
+	public static void jsonToMap() {
+
+		JSONObject dataObj = new JSONObject();
+		dataObj.put("status", "status 11");
+		dataObj.put("registerMobile", "registerMobile 22");
+		dataObj.put("globalId", "globalId 33");
+		dataObj.put("name", "name 44");
+		dataObj.put("reservedMobile", "reservedMobile 55");
+		dataObj.put("bankAccountName", "bankAccountName 66");
+		dataObj.put("bankAccountNo", "bankAccountNo 77");
+		dataObj.put("bankName", "bankName 88");
+		dataObj.put("bankNo", "bankNo 99");
+
+		JSONObject isRegisteredObj = new JSONObject();
+		isRegisteredObj.put("code", "code code");
+		isRegisteredObj.put("msg", "msg msg");
+		isRegisteredObj.put("data", dataObj);
+
+		String jsonStr = isRegisteredObj.toJSONString();
+		System.out.println(jsonStr);
+
+		Map maps = (Map) JSON.parse(jsonStr);
+		System.out.println(maps.get("data"));
 	}
 
 }
