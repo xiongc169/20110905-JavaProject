7ea9146a2a7e9fa4017573154038f45c40feb31f
xiongchao <xiongchao@zafh.com.cn>
2018-05-03 21:45:23 +0800

    stream flatMap - 2018年5月3日21:45:20

---
+++ JavaProject/parent/practice-java8/src/main/java/org/practice/java8/book02/ch0502/StreamOperation.java
@@ -0,0 +1,32 @@
+package org.practice.java8.book02.ch0502;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author 20180112002
+ *
+ * @description StreamOperation.java
+ *
+ * @date 2018年5月3日
+ *
+ * @version 1.0
+ *
+ */
+public class StreamOperation {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static void flatMapTest() {
+		List<String> hello = new ArrayList<String>();
+		hello.add("Hello");
+		hello.add("World");
+
+		List<String> chara = hello.stream().map(item -> item.split("")).flatMap(Arrays::stream).distinct().collect(Collectors.toList());
+	}
+
+}
