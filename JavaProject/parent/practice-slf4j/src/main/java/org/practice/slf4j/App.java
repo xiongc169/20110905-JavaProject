e6d73a7399e5a1279422e212fb9c2010b392dba3
xiongchao <xiongchao@zafh.com.cn>
2018-07-24 17:26:43 +0800

    slf4j - 2018年7月24日17:26:40

--- JavaProject/parent/practice-slf4j/src/main/java/org/practice/slf4j/App.java
+++ JavaProject/parent/practice-slf4j/src/main/java/org/practice/slf4j/App.java
@@ -1,13 +1,22 @@
 package org.practice.slf4j;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+		// Logger log = LoggerFactory.getLogger(App.class);
+		// log.debug("debug");
+		// log.info("info");
+		// log.error("error");
+
+		Logger logger = LoggerFactory.getLogger(App.class);
+		logger.info("Hello World");
+		System.out.println("Hello World!");
+	}
 }
