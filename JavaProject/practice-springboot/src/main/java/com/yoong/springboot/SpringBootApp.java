ebf23d3f29b34265b8c1ca49398e3f57b6638d25
熊超 <xiongchao@fincs.com.cn>
2019-04-29 10:34:57 +0800

    maven2springBoot

--- JavaProject/practice-springboot/src/main/java/com/yoong/springboot/SpringBootApp.java
+++ JavaProject/practice-springboot/src/main/java/com/yoong/springboot/SpringBootApp.java
@@ -3,6 +3,8 @@ package com.yoong.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,23 +17,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @date 2018年9月20日
  *
  */
-@SpringBootApplication
+//@SpringBootApplication
+//@Configuration
+@EnableAutoConfiguration
+//@ComponentScan(basePackages = "com.yoong.*")
+@Controller
 public class SpringBootApp {
 
-	/**
-	 * 入口函数
-	 * 
-	 * @param args
-	 * @throws Exception
-	 *
-	 */
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SpringBootApp.class, args);
-	}
+    /**
+     * 入口函数
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
 
-	@ResponseBody
-	@RequestMapping("/app/home")
-	public String home() {
-		return "Hello World!";
-	}
+    /**
+     * http://localhost:8085/app/home
+     * 成功：@Controller + @EnableAutoConfiguration
+     * 成功：@Controller + @EnableAutoConfiguration + @ComponentScan
+     * 失败：@SpringBootApplication
+     * 成功：@Controller + @SpringBootApplication
+     *
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/app/home")
+    public String home() {
+        return "Hello World!";
+    }
 }
--- JavaProject/practice-springboot/src/main/java/com/yoong/springboot/controller/UserController.java
+++ JavaProject/practice-springboot/src/main/java/com/yoong/springboot/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.yoong.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,17 +16,30 @@ import com.yoong.springboot.config.AuthConfig;
  * @date 2018年9月20日
  *
  */
+@EnableAutoConfiguration
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-	@Autowired
-	private AuthConfig config;
+    @Autowired
+    private AuthConfig config;
 
-	@ResponseBody
-	@RequestMapping("/login")
-	public String login(String name) {
-		return "Hello " + name + config.getUserName();
-	}
+    /**
+     * http://localhost:8085/user/login
+     * 失败：@Controller + @EnableAutoConfiguration
+     * 成功：@Controller + @EnableAutoConfiguration + @ComponentScan
+     * 成功：@SpringBootApplication
+     * 成功：@Controller + @SpringBootApplication
+     *
+     * 失败：@Controller + @EnableAutoConfiguration
+     *
+     * @param name
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/login")
+    public String login(String name) {
+        return "Hello " + name + config.getUserName();
+    }
 
 }
