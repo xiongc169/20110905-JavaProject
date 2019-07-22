7fb859156a73d7f64cdac8f3f7e6e25dd42d3b6c
熊超 <xiongchao@fincs.com.cn>
2019-07-22 21:26:49 +0800

    kafka

---
+++ JavaProject/parent-component/practice-kafka/src/main/java/com/yoong/practice/AppKafka.java
@@ -0,0 +1,13 @@
+package com.yoong.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppKafka
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
