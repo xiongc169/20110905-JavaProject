8c4e252ced3fc88b480892dae8eed1ca0b0f97a8
熊超 <xiongchao@fincs.com.cn>
2019-08-21 11:05:29 +0800

    dubbo

--- JavaProject/parent-dubbo/dubbo-consumer/src/main/java/org/dubbo/consumer/Consumer.java
+++ JavaProject/parent-dubbo/dubbo-consumer/src/main/java/org/dubbo/consumer/Consumer.java
@@ -1,5 +1,8 @@
 package org.dubbo.consumer;
 
+import com.chesheng.decision.api.IDeipPrefixFacade;
+import com.chesheng.decision.request.OrderQueryRequest;
+import com.chesheng.decision.response.OrderQueryResponse;
 import org.dubbo.common.service.CalculatorService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
@@ -8,23 +11,40 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
  * Hello world!
  */
 public class Consumer {
+
+    /**
+     * 入口函数
+     */
     public static void main(String[] args) {
         try {
-
-            FileSystemXmlApplicationContext fsContext = new FileSystemXmlApplicationContext(new String[]{"classpath:consumer.xml"});
-            CalculatorService calculator = (CalculatorService) fsContext.getBean("advancedCalculatorService");//calculatorService、advancedCalculatorService
-            double result = calculator.add(100, 200);
-            double result1 = calculator.sub(100, 200);
-            double result2 = calculator.mul(100, 200);
-            double result3 = calculator.div(100, 200);
-            System.out.println(result);
-            System.out.println(result1);
-            System.out.println(result2);
-            System.out.println(result3);
-
-            ClassPathXmlApplicationContext cpContext = new ClassPathXmlApplicationContext("classpath:consumer.xml");
+            localTest();
+            deipTest();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void localTest() {
+        FileSystemXmlApplicationContext fsContext = new FileSystemXmlApplicationContext(new String[]{"classpath:consumer.xml"});
+        CalculatorService calculator = (CalculatorService) fsContext.getBean("advancedCalculatorService");//calculatorService、advancedCalculatorService
+        double result = calculator.add(100, 200);
+        double result1 = calculator.sub(100, 200);
+        double result2 = calculator.mul(100, 200);
+        double result3 = calculator.div(100, 200);
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
+
+    public static void deipTest() {
+        FileSystemXmlApplicationContext fsContext = new FileSystemXmlApplicationContext(new String[]{"classpath:consumer.xml"});
+        IDeipPrefixFacade deipFacade = (IDeipPrefixFacade) fsContext.getBean("deipFacade");//calculatorService、advancedCalculatorService
+        OrderQueryRequest request = new OrderQueryRequest();
+        request.setAppId("cls");
+        request.setOrderNo("706328");
+        request.setBizType(1);
+        OrderQueryResponse response = deipFacade.getNewestByOrderNo(request);
+        System.out.println(response.getData());
     }
 }
