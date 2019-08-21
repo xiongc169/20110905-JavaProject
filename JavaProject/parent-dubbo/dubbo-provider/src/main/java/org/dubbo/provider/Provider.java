6c7fa96b711ca202fa29c2f05f67db008eeacf5b
熊超 <xiongchao@fincs.com.cn>
2019-08-21 11:03:48 +0800

    dubbo

--- JavaProject/parent-dubbo/dubbo-provider/src/main/java/org/dubbo/provider/Provider.java
+++ JavaProject/parent-dubbo/dubbo-provider/src/main/java/org/dubbo/provider/Provider.java
@@ -5,21 +5,22 @@ import java.util.Scanner;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Provider {
-	public static void main(String[] args) {
 
-		try {
-			FileSystemXmlApplicationContext fsContext = new FileSystemXmlApplicationContext(new String[] { "classpath:provider.xml" });
-			fsContext.start();
-			System.out.println("启动成功！");
+    /**
+     * 入口函数
+     */
+    public static void main(String[] args) {
+        try {
+            FileSystemXmlApplicationContext fsContext = new FileSystemXmlApplicationContext(new String[]{"classpath:provider.xml"});
+            fsContext.start();
+            System.out.println("启动成功！");
 
-			Scanner scanner = new Scanner(System.in);
-			int input = scanner.nextInt();
-			System.out.println(input);
-
-			System.in.read();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+            System.out.println(input);
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
