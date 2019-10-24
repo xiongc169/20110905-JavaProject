13ae713a490f9edab2673cf41840a5aaa2d61abf
熊超 <xiongchao@fincs.com.cn>
2019-10-24 15:42:29 +0800

    springx

---
+++ JavaProject/parent-persistence/mybatis-springx/src/main/java/com/yoong/practice/App.java
@@ -0,0 +1,13 @@
+package com.yoong.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
