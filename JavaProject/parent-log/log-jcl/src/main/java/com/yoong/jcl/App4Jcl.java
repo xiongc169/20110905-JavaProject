e5265cafe10a9dc8a468d47209529f737cecc62c
熊超 <xiongchao@fincs.com.cn>
2019-11-27 21:07:11 +0800

    jcl

--- JavaProject/parent-log/log-jcl/src/main/java/com/yoong/jcl/App4Jcl.java
+++ JavaProject/parent-log/log-jcl/src/main/java/com/yoong/jcl/App4Jcl.java
@@ -1,13 +1,32 @@
 package com.yoong.jcl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
- * Hello world!
- *
+ * App4Jcl
  */
-public class App4Jcl
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App4Jcl {
+
+    private static Log log = LogFactory.getLog(App4Jcl.class);
+
+    /**
+     * 入口函数
+     */
+    public static void main(String[] args) {
+        jclDemo();
+        System.out.println("App4Jcl Ending");
+    }
+
+    public static void jclDemo() {
+        //不添加log4j依赖，类型为：org.apache.commons.logging.impl.Jdk14Logger
+        //添加log4j依赖，类型为：org.apache.commons.logging.impl.Log4JLogger
+        System.out.println(log.getClass());
+        log.trace("App4Jcl.trace");
+        log.debug("App4Jcl.debug");
+        log.info("App4Jcl.info");
+        log.warn("App4Jcl.warn");
+        log.error("App4Jcl.derrorebug");
+        log.fatal("App4Jcl.fatal");
     }
 }
