88b766bf7a09a8245b7dcb4edc575f12d9b883ff
xiongchao <xiongchao@fincs.com.cn>
2020-05-28 16:48:45 +0800

    rocketmq

---
+++ JavaProject/parent-component/component-rocketmq/src/main/java/com/yoong/practice/App.java
@@ -0,0 +1,17 @@
+package com.yoong.practice;
+
+/**
+ * Hello world!
+ */
+public class App {
+    /**
+     * 入口函数
+     */
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hello World!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
