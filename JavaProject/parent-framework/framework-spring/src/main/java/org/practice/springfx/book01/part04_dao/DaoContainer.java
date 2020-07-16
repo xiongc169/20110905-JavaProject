7ac286fcd9cfb6e4a90dfff3705c6f00c5601e58
xiongchao <xiongchao@fincs.com.cn>
2020-07-16 16:00:08 +0800

    dao

---
+++ JavaProject/parent-framework/framework-spring/src/main/java/org/practice/springfx/book01/part04_dao/DaoContainer.java
@@ -0,0 +1,102 @@
+package org.practice.springfx.book01.part04_dao;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.practice.springfx.book01.part04_dao.ch1401.JdbcManager;
+import org.practice.springfx.book01.part04_dao.ch1401.MyJdbcDaoSupport;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.StatementCallback;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+/**
+ * @Desc DaoContainer
+ * <p>
+ * @Author yoong
+ * <p>
+ * @Date 2018年8月20日
+ * <p>
+ * @Version 1.0
+ */
+public class DaoContainer {
+
+    /**
+     * 入口函数
+     */
+    public static void main(String[] args) {
+        try {
+            JdbcManager.jdbc140101();
+            jdbcTemplate140102();
+            datasource140104();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * TODO: 14.1.2、JdbcTemplate的诞生
+     * TODO: 14.1.3、JdbcTemplate和它的兄弟们
+     */
+    public static void jdbcTemplate140102() throws Exception {
+        String mysqlUrl = "jdbc:mysql://127.0.0.1:3306/mysql?serverTimezone=UTC";
+        String username = "root";
+        String password = "123456";
+        String querySql = "SELECT * FROM `test`";
+        String querySql01 = "SELECT * FROM `test` LIMIT 1";
+        DataSource dataSource = new DriverManagerDataSource(mysqlUrl, username, password);
+        //Connection connection = dataSource.getConnection();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(mysqlUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+        basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        //创建
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        //RowMapper 的使用
+        List<String> queryResult = jdbcTemplate.query(querySql, new RowMapper<String>() {
+            @Override
+            public String mapRow(ResultSet resultSet, int i) throws SQLException {
+                String column01 = resultSet.getString(1);
+                System.out.println(column01);
+                return column01;
+            }
+        });
+        System.out.println(queryResult.size());
+
+        //StatementCallback 的使用
+        Object executeResult = jdbcTemplate.execute(new StatementCallback<Object>() {
+            @Override
+            public Object doInStatement(Statement statement) throws SQLException, DataAccessException {
+                boolean result = statement.execute(querySql01);
+                return result;
+            }
+        });
+        System.out.println(executeResult);
+
+        //使用 DataSourceUtils 管理 Connection
+        Connection connection02 = DataSourceUtils.getConnection(dataSource);
+        System.out.println(connection02);
+    }
+
+    /**
+     * TODO: 14.1.4、Spring中的DataSource
+     */
+    public static void datasource140104() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        SingleConnectionDataSource singleConnectionDataSource = new SingleConnectionDataSource();
+        BasicDataSource basicDataSource = new BasicDataSource();
+
+        MyJdbcDaoSupport myJdbcDaoSupport = new MyJdbcDaoSupport();
+        DataSource dataSource = myJdbcDaoSupport.getDataSource();
+    }
+}
---
+++ JavaProject/parent-framework/framework-spring/src/main/java/org/practice/springfx/book01/part04_dao/ch1401/MyJdbcDaoSupport.java
@@ -0,0 +1,15 @@
+package org.practice.springfx.book01.part04_dao.ch1401;
+
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+/**
+ * @Desc MyJdbcDaoSupport
+ * <p>
+ * @Author yoong
+ * <p>
+ * @Date 2018年8月20日
+ * <p>
+ * @Version 1.0
+ */
+public class MyJdbcDaoSupport extends JdbcDaoSupport {
+}
