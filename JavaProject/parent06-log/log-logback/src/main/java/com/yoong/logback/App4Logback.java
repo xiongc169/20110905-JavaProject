e7a27886dfc73a25e5fa3457e783620381c78919
xiongchao <xiongchao@fincs.com.cn>
2021-02-07 11:48:53 +0800

    parent06-log

---
+++ JavaProject/parent06-log/log-logback/src/main/java/com/yoong/logback/App4Logback.java
@@ -0,0 +1,33 @@
+package com.yoong.logback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Desc App4Logback
+ * <p>
+ * @Author yoong
+ * <p>
+ * @Date 2019年11月22日
+ * <p>
+ * @Version 1.0
+ */
+public class App4Logback {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(App4Logback.class);
+
+    /**
+     * 入口函数
+     */
+    public static void main(String[] args) {
+        logbackDemo();
+        System.out.println("logback Ending");
+    }
+
+    public static void logbackDemo() {
+        LOGGER.debug("logback的--debug日志--输出");
+        LOGGER.info("logback的--info日志--输出");
+        LOGGER.warn("logback的--warn日志--输出");
+        LOGGER.error("logback的--error日志--输出");
+    }
+}
---
+++ JavaProject/parent06-log/log-slf4j/src/main/java/com/yoong/slf4j/AppSlf4j.java
@@ -0,0 +1,33 @@
+package com.yoong.slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Desc AppSlf4j
+ * <p>
+ * @Author yoong
+ * <p>
+ * @Date 2019年11月22日
+ * <p>
+ * @Version 1.0
+ */
+public class AppSlf4j {
+
+    private static Logger log = LoggerFactory.getLogger(AppSlf4j.class);
+
+    /**
+     * 入口函数
+     */
+    public static void main(String[] args) {
+        slf4jDemo();
+        System.out.println("SLF4J Ending");
+    }
+
+    public static void slf4jDemo() {
+        log.debug("This is SLF4J DEBUG 2018-7-25 11:19:59");
+        log.info("This is SLF4J INFO 2018-7-25 11:20:10");
+        log.warn("This is SLF4J WARN 2018-7-25 11:20:16");
+        log.error("This is SLF4J ERROR 2018-7-25 11:20:21");
+    }
+}
